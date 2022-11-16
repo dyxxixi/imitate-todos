d84de49f5378a540aa0c30d9794d85426ab3005a
d-yx <352104389@qq.com>
2022-11-16 10:51:25 +0800

    feat:finish  html css

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,53 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="todoapp">
+    <header class="header">
+      <h1>任务清单</h1>
+      <input class="new-todo" placeholder="需要做什么？">
+    </header>
+    <main class="main">
+      <input id="toggle-all" class="toggle-all" type="checkbox">
+      <label for="toggle-all">全部标记为完成</label>
+      <ul class="todo-list">
+        <li class="todo">
+          <div class="view">
+            <input class="toggle" type="checkbox">
+            <label>coding</label>
+            <button class="destroy"></button>
+          </div>
+          <input class="edit" type="text">
+        </li>
+        <li class="todo">
+          <div class="view">
+            <input class="toggle" type="checkbox">
+            <label>coding</label>
+            <button class="destroy"></button>
+          </div>
+          <input class="edit" type="text">
+        </li>
+      </ul>
+    </main>
+    <footer class="footer">
+      <span class="todo-count">
+        <span>
+          剩下
+        </span>
+        <strong>
+          1
+        </strong>
+        <span>
+          个任务
+        </span>
+      </span>
+      <ul class="filters">
+        <li><a href="#/all" class="selected">全部</a></li>
+        <li><a href="#/active">未完成</a></li>
+        <li><a href="#/completed">已完成</a></li>
+      </ul>
+      <button class="clear-completed">清除已完成</button>
+    </footer>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
